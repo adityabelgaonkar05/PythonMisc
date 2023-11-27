5aab3ba920eab414cec9119f9e6d7682066c136c
adityabelgaonkar05 <adityabelgaonkar.30@gmail.com>
2023-11-27 20:38:56 +0530

    ESEPractice added.

---
+++ ESEPractice/MatPlotLib/plotQuadraticEquation.py
@@ -0,0 +1,11 @@
+#Q2 B (I dont remember full)
+#we have to plot the line or curve for the quadratic equation in range [-5: 5] and plot a bar chart for some company
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([i for i in range(-5, 6)])
+y = np.array([i**2 for i in range(-5, 6)])
+
+plt.plot(x, y)
+plt.show()
+
