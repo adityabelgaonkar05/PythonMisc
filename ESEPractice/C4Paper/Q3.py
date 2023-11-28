bb0355a91d98e74ab0d116949d17505e7eef49cb
adityabelgaonkar05 <adityabelgaonkar.30@gmail.com>
2023-11-28 22:09:46 +0530

    Q2 added from C4 odd set.

---
+++ ESEPractice/C4Paper/Q3.py
@@ -0,0 +1,9 @@
+#WAP to read the A.txt fil. Prepare the A.txt with alphanumeric characters. 
+#Cut all digits from file A.txt into another file B.txt. 
+#Replace the cut digits to X. Display both file contents.
+
+# if A.txt--> I am Rajesh. I am 18 years old. 
+# after operation
+# A.txt--> I am Rajesh. I am X years old.
+# B.txt--> 18
+
