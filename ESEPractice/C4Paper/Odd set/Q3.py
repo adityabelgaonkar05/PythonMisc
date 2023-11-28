80edf5da01d872484b08994a09f4f35dd88ba264
adityabelgaonkar05 <adityabelgaonkar.30@gmail.com>
2023-11-29 00:56:32 +0530

    Completed ALL Questions.
    Only the common Q4 in ODD and Q3 in EVEN left.

--- ESEPractice/C4Paper/Odd set/Q3.py
+++ ESEPractice/C4Paper/Odd set/Q3.py
@@ -7,3 +7,36 @@
 # A.txt--> I am Rajesh. I am X years old.
 # B.txt--> 18
 
+f1 = open("A.txt", "r")
+s = f1.read().split()
+f1.close()
+content = ""
+
+for i in s:
+    if i.isnumeric():
+        content+="X"
+        content+=" "
+        f2 = open("B.txt", "a")
+        f2.write(i + " ")
+        f2.close()
+        
+    else: 
+        content+=i
+        content+=" "
+
+f1 = open("A.txt", "w")
+f1.write(content)
+f1.close()
+
+f1 = open("A.txt", "r")
+Acontent = f1.read()
+f1.close()
+
+f2 = open("B.txt", "r")
+Bcontent = f2.read()
+f2.close()
+
+print(Acontent)
+print(Bcontent)
+
+    
