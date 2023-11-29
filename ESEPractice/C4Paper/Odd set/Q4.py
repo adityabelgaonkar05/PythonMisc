e1813c285318cac494b3c78999a23bfd13d274b6
adityabelgaonkar05 <adityabelgaonkar.30@gmail.com>
2023-11-29 19:36:05 +0530

    Added all questions from C4 paper.

---
+++ ESEPractice/C4Paper/Odd set/Q4.py
@@ -0,0 +1,33 @@
+# WAP to read the working hours of n number of employees with 4 weeks data 
+# plans using numpy. Sample shown below
+# find-->Shape of employee duty chart
+#     -->Sum of hours in month
+#     -->Search employeee securing highest working hours
+
+# Name of Employee    week1   week2   week3   week4
+# VIPUL                40       40     40      40
+# RISHI                42       42     42      42
+# VIVEK                45       45     45      45
+# VIBHU                38       38     38      38
+# VIDITA               30       30     30      30
+
+import numpy as np
+
+names = ["VIPUL", "RISHI", "VIVEK", "VIBHU", "VIDITA"]
+week1 = [40, 42, 45, 38, 30]
+week2 = week3 = week4 = week1
+
+arr = np.array([names, week1,week2, week3, week4])
+
+print(np.shape(arr))
+
+sumOfWeeks = [0]*len(names)
+
+for i in range(4): #I have used since the question very specifically mentions 4 weeks
+    sumOfWeeks[i] += int(arr[i+1][0])
+
+print(f"{names}\n{sumOfWeeks}")
+
+maxWorkingHours = np.argmax(arr)
+
+print(f"Max number of working hours are for: {names[maxWorkingHours]}")
