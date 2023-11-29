938c7a0d0eec270dd80d702d59b0ec07b34c30ce
adityabelgaonkar05 <adityabelgaonkar.30@gmail.com>
2023-11-30 01:27:07 +0530

    C5 paper full added and updated P2 paper.

---
+++ ESEPractice/C5/Odd set/Q4.py
@@ -0,0 +1,31 @@
+#Write a fucntion AMCount() in Python, which should read each character 
+#of a text file STORY.txt, should count and display the occurance 
+#of alphabets A and M (small cases too)
+
+def AMCount(file):
+    contentoffile = file.read()
+    numberofA = 0
+    numberofM = 0
+
+    for i in range(len(contentoffile)):
+        if contentoffile[i].lower() == "a":
+            numberofA += 1
+
+        elif contentoffile[i].lower() == "m":
+            numberofM += 1
+
+    return numberofA, numberofM
+
+try:
+    f = open("STORY.txt", "r")
+    f.close()
+
+except:
+    f = open("STORY.txt", "w")
+    f.close()
+
+f = open("STORY.txt", "r")
+
+A, M = AMCount(f)
+
+print(f"A-> {A}, M-> {M}")
